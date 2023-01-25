2b2a021410f9c565efb40cdc5e6183c07286324c
Gabriela Garcia Serrano <118684159+GabrielaSpace@users.noreply.github.com>
2023-01-25 18:22:09 +0100

    finished details

--- app.js
+++ app.js
@@ -1,38 +1,24 @@
 const express = require('express')
 const cowsay = require('cowsay')
-const calculator = require('./utils/calculator')
 const error404 = require('./middlewares/error404')
 const morgan = require('morgan')
-
+const dotenv = require('dotenv')
+dotenv.config()
 // Módulos de Rutas
-const booksRoutes = require('./routes/booksRoutes')
-const productsRoutes = require('./routes/productsRoutes')
-const productsApiRoutes = require('./routes/productsApiRoutes')
+
 const entriesApiRoutes = require('./routes/entriesApiRoutes')
 const authorsApiRoutes = require('./routes/authorApiRoutes')
 
 const app = express()
 const port = 3000
 
-// Template engine
-app.set('view engine', 'pug');
-app.set('views', './views');
-
 // Middlewares
 app.use(express.json()); // Habilitar tipo de dato a recibir
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'))
-
-app.get('/', (req, res) => {
-    const calc = calculator.add(2, 2);
-    //res.send(`Hello World! La suma es ${suma}`)
-    res.render('content', { msj: "The Bridge", calc })
-})
+app.use(morgan)
 
 //Rutas 
-app.use('/books',booksRoutes); // Books
-app.use('/products',productsRoutes); // Rutas web products
-app.use('/api/products',productsApiRoutes); // Rutas web API products
+
 app.use('/api/entries',entriesApiRoutes); // Rutas API entries
 app.use('/api/author', authorsApiRoutes);// Rutas API authors
 app.use(error404); // Middleware Para ruta no encontrada (404)
--- controllers/entriesApiController.js
+++ controllers/entriesApiController.js
@@ -1,13 +1,5 @@
 const entry = require('../models/entries'); // Importar el modelo de la BBDD
 
-//getEntries
-// if(hay email)
-//     busca por mail
-// else
-//     busca todo
-
-
-// GET http://localhost:3000/entries --> ALL
 // GET http://localhost:3000/entries?email=hola@gmail.com --> por email
 const getEntries = async (req, res) => {
     let entries;
@@ -20,13 +12,6 @@ const getEntries = async (req, res) => {
     res.status(200).json(entries); // [] con las entries encontradas
 }
 
-//createEntry
-// POST http://localhost:3000/api/entries
-// let newEntry = {
-//     title:"noticia desde Node",
-//     content:"va a triunfar esto2",
-//     email:"alejandru@thebridgeschool.es",
-//     category:"sucesos"}
 
 // Crear entry por email
 const createEntry = async (req, res) => {
